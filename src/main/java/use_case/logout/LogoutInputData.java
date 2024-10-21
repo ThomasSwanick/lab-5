512c18f5c84292a2bda243afa8ab63d304b591d9
JonDD-D <dengjohn123@163.com>
2024-10-21 18:03:35 -0400

    eidition for logout

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -4,9 +4,33 @@ package use_case.logout;
  * The Input Data for the Logout Use Case.
  */
 public class LogoutInputData {
+    private String username;
+    private String password;
 
     public LogoutInputData(String username) {
         // TODO: save the current username in an instance variable and add a getter.
+        this.username = username;
+        password = null;
     }
 
+    public LogoutInputData(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
